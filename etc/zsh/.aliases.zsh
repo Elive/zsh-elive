d85f89109e825e86ef4b5a4f4eb50ccfaaa5be89
Thanatermesis <thanatermesis@gmail.com>
2014-02-02 14:49:14 +0100

    Dot expansion interactively, instead of "....."

--- etc/zsh/.aliases.zsh
+++ etc/zsh/.aliases.zsh
@@ -1,27 +1,3 @@
-# warning: this can be unsecure?
-#alias -g ...=../..
-#alias -g ....=../../..
-#alias -g .....=../../../..
-#alias -g ......=../../../../..
-#alias -g .......=../../../../../..
-#alias -g ........=../../../../../../..
-#Alternative, thanks to Fenhl
-_rationalise-dot() {
-    if [[ $LBUFFER == "cd "* ]] || [[ $LBUFFER =~ "[^ ]*" ]]; then
-	if [[ $LBUFFER = *.. ]]; then
-	    LBUFFER+=/..
-	elif [[ $LBUFFER == "cd." ]]; then
-	    LBUFFER="cd .."
-	else
-	    LBUFFER+=.
-	fi
-    else
-	LBUFFER+=.
-    fi
-}
-zle -N _rationalise-dot
-bindkey . _rationalise-dot
-
 # apt* related stuff
 function apir(){
     sudo -- sh -c "apt-get update ; rm -f /var/cache/apt/archives/${1}_*.deb ; apt-get install --reinstall $@"
--- etc/zsh/zshrc.elive.zsh
+++ etc/zsh/zshrc.elive.zsh
@@ -13,3 +13,21 @@ if [[ -s "/etc/zsh/.aliases.zsh" ]] ; then
     source "/etc/zsh/.aliases.zsh"
 fi
 
+
+# Dot substitution as you type, much better than the classic ... aliases
+_rationalise-dot() {
+    if [[ $LBUFFER == "cd "* ]] || [[ $LBUFFER =~ "[^ ]*" ]]; then
+	if [[ $LBUFFER = *.. ]]; then
+	    LBUFFER+=/..
+	elif [[ $LBUFFER == "cd." ]]; then
+	    LBUFFER="cd .."
+	else
+	    LBUFFER+=.
+	fi
+    else
+	LBUFFER+=.
+    fi
+}
+zle -N _rationalise-dot
+bindkey . _rationalise-dot
+
