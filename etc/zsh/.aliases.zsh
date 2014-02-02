7481820b049cb46fa428799e30a2292ed3112bb3
Thanatermesis <thanatermesis@gmail.com>
2014-02-02 14:45:20 +0100

    alternative to multiple dots

--- etc/zsh/.aliases.zsh
+++ etc/zsh/.aliases.zsh
@@ -1,10 +1,26 @@
 # warning: this can be unsecure?
-alias -g ...=../..
-alias -g ....=../../..
-alias -g .....=../../../..
-alias -g ......=../../../../..
-alias -g .......=../../../../../..
-alias -g ........=../../../../../../..
+#alias -g ...=../..
+#alias -g ....=../../..
+#alias -g .....=../../../..
+#alias -g ......=../../../../..
+#alias -g .......=../../../../../..
+#alias -g ........=../../../../../../..
+#Alternative, thanks to Fenhl
+_rationalise-dot() {
+    if [[ $LBUFFER == "cd "* ]] || [[ $LBUFFER =~ "[^ ]*" ]]; then
+	if [[ $LBUFFER = *.. ]]; then
+	    LBUFFER+=/..
+	elif [[ $LBUFFER == "cd." ]]; then
+	    LBUFFER="cd .."
+	else
+	    LBUFFER+=.
+	fi
+    else
+	LBUFFER+=.
+    fi
+}
+zle -N _rationalise-dot
+bindkey . _rationalise-dot
 
 # apt* related stuff
 function apir(){
