0a6701b1bc3547a6c00ec6f57a2d259205a01139
Thanatermesis <thanatermesis@gmail.com>
2014-02-02 14:27:04 +0100

    improved aliases

--- etc/skel/.aliases.bash
+++
@@ -1,5 +0,0 @@
-# apt* related stuff
-function apir(){
-    sudo -- sh -c "apt-get update ; rm -f /var/cache/apt/archives/${1}_*.deb ; apt-get install --reinstall $@"
-}
-
--- etc/zsh/.aliases.zsh
+++ etc/zsh/.aliases.zsh
@@ -11,3 +11,7 @@ function apir(){
     sudo -- sh -c "apt-get update ; rm -f /var/cache/apt/archives/${1}_*.deb ; apt-get install --reinstall $@"
 }
 
+# this is useful for reload ZSH conf
+alias XX="source $HOME/.zshrc"
+
+
