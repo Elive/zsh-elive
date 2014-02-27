55ad58c947c13439bc6c33326093ba80d7ebe30d
Thanatermesis <thanatermesis@gmail.com>
2014-02-27 05:31:25 +0100

    include profile.d for zsh too

--- etc/zsh/zprofile.elive.zsh
+++ etc/zsh/zprofile.elive.zsh
@@ -3,3 +3,12 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile" ]]; then
   source "${ZDOTDIR:-$HOME}/.zprezto/runcoms/zprofile"
 fi
 
+if [ -d /etc/zsh/profile.d ]; then
+  for i in /etc/zsh/profile.d/*sh ; do
+    if [ -r $i ]; then
+      . $i
+    fi
+  done
+  unset i
+fi
+
