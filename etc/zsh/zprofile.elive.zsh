37de5ff06a3f7ca913b4f012e7fae8d8f8bad6d3
Thanatermesis <thanatermesis@gmail.com>
2014-09-21 15:27:10 +0200

    use -r just like how bash does the same

--- etc/zsh/zprofile.elive.zsh
+++ etc/zsh/zprofile.elive.zsh
@@ -6,7 +6,7 @@ fi
 if [ -d /etc/zsh/zprofile.d ]; then
   for i in /etc/zsh/zprofile.d/*.sh ; do
       # note: do not use "if executable", some ones are not, like the one put by live-build scripts
-      if [ -s "$i" ]; then
+      if [ -r "$i" ]; then
           zsh "$i"
       fi
   done
