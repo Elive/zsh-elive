4df91c4aa438170bbeca4f5240088ef11b69c71f
Thanatermesis <thanatermesis@gmail.com>
2014-10-05 20:39:47 +0200

    added useful alias for eet

--- etc/zsh/.aliases.zsh
+++ etc/zsh/.aliases.zsh
@@ -3,6 +3,10 @@ function apir(){
     sudo -- sh -c "apt-get update ; rm -f /var/cache/apt/archives/${1}_*.deb ; apt-get install --reinstall $@"
 }
 
+function eet_conf_unpack_all_files(){
+    find . -mindepth 1 -maxdepth 1 -type f -iname '*'.cfg -exec eet -d {} config {}.src \; ;
+}
+
 # this is useful for reload ZSH conf
 alias XX="source $HOME/.zshrc"
 
