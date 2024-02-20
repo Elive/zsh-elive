8f6818899e10a12dafeee04f1ebdeffc266e3e6b
Thanatermesis <thanatermesis@gmail.com>
2024-02-20 09:42:38 -0500

    fix for manpath command not found

--- etc/zsh/zshrc.elive.zsh
+++ etc/zsh/zshrc.elive.zsh
@@ -208,7 +208,9 @@ NPM_PACKAGES="$HOME/.npm-packages"
 path=($HOME/.npm-packages/bin(N) $path)
 # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
 unset MANPATH  # delete if you already modified MANPATH elsewhere in your configuration
-MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+if [[ -x /usr/bin/manpath ]] ; then
+    MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
+fi
 
 # Tell Node about these packages
 NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
