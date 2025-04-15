104e8d11ba7c34e9c7faf054178ada814b4f5045
Thanatermesis <thanatermesis@gmail.com>
2025-04-15 14:57:52 -0400

    improved fetching of variable entries

--- etc/zsh/zprofile.elive.zsh
+++ etc/zsh/zprofile.elive.zsh
@@ -15,5 +15,6 @@ fi
 
 # Set some environment variables, like if the user has set OPENAI_API_KEY on it
 if [ -s ~/.profile ] ; then
-  eval "$( grep -a --color=never "^export " ~/.profile )"
+  eval "$( grep --color=never -a -P '^(?!.*(PATH|SHELL|SH)=)[[:space:]]*((export[[:space:]]+)?[[:alnum:]_]+=(".*"|'"'"'.*'"'"'|.*[^[:space:]]))' ~/.profile )"
+
 fi
